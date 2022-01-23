af94114780d5f13e40a0e273be4db799bbecbf29
Fauzi Kurniawan <ozilpym123@gmail.com>
2022-01-23 22:57:39 +0700

    tmbah main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Semua sintaksis dasar bahasa pemograman terdiri dari :
+1. Sekuensual : Langkah berurutan
+2. Percabangan : Langkah melompat jika kondisi terpenuhi
+3. Perulangan : Mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
+"""
+
+# Sekuensial
+
+print('Ibu berkata,"Pegi ke toko"')
+print('Budi menjawab,"Baik, apa yang harus saya lakukan di toko?"')
+print('Ibu menjawab,"Beli satu botol susu,jika ada telur beli 6"')
+print('Maka Budi berangkat ke toko')
+print('Dan mulai berbelanja')
