25761715207f4d4321055ed71fb05e3abdfe098f
Fauzi Kurniawan <ozilpym123@gmail.com>
2022-01-24 08:33:41 +0700

    sintaksis percabangan

--- main.py
+++ main.py
@@ -12,3 +12,24 @@ print('Budi menjawab,"Baik, apa yang harus saya lakukan di toko?"')
 print('Ibu menjawab,"Beli satu botol susu,jika ada telur beli 6"')
 print('Maka Budi berangkat ke toko')
 print('Dan mulai berbelanja')
+
+# Percabangan
+
+jumlah_botol_susu = 173
+jumlah_telur = 342
+print(f"jumlah botol susu {jumlah_botol_susu} botol")
+print(f"jumlah telur {jumlah_telur} butir")
+
+if jumlah_botol_susu > 0:
+    print('Budi mengecek harganya,dan uangnya cukup')
+    if jumlah_telur == 0:
+        print("Budi membeli 1 botol susu")
+    elif jumlah_telur <= 6:
+        print(f'Budi membeli 1 botol susu dan {jumlah_telur} butir telur')
+    else :
+        print('Budi membeli 1 botol susu dan 6 butir telur')
+
+else:
+    print('Budi tidak jadi membeli 1 botol susu')
+print('Budi pulang ke rumah')
+print('Dan menyampaikan hasil belanja kepada ibu')
