4858ab9377c75ee765a168b7e12e7388c0053063
Fauzi Kurniawan <ozilpym123@gmail.com>
2022-01-24 20:23:41 +0700

    sintaksis perulangan

--- Python_Perulangan.py
+++ Python_Perulangan.py
@@ -2,7 +2,12 @@
 Program perulangan membaca buku
 """
 
-jumlah_buku = 0
+jumlah_buku = 10
 jumlah_buku_yang_sudah_dibaca = 0
-
 print('Ibu berkata,"Baca semua bukumu."')
+print(f'jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
+
+for jumlah_buku_yang_sudah_dibaca in range(jumlah_buku_yang_sudah_dibaca+1, jumlah_buku+1):
+    print(f'Buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca')
+
+print(f'jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
