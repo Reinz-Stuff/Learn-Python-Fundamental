c179ff7cac331d6d39c414e4648a5f7e2e85f53a
Fauzi <fauzik354313@gmail.com>
2022-05-19 13:19:03 +0700

    Dict Komplek

--- 7. Dictionary.py
+++ 7. Dictionary.py
@@ -1,10 +1,26 @@
+# JSON wajib menggunakan petik dua
 users = {
-    'id': 1,
-    'name': 'Leanne Graham',
-    'username': 'Brett',
-    'email': 'sincere@april.biz'  # email : key, tidak boleh angka harus string
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Brett",
+    "email": "sincere@april.biz",  # email : key, tidak boleh angka harus string
+    "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496"
+        }
+
+    }
 }
 print(users)
-print(users['name'])
-print(users['username'])
-print(users['email'])
+print(users["name"])
+print(users["username"])
+print(users["email"])
+print(users["address"])
+print(users["address"]['street'])
+print(users["address"]['geo'])
+print(users["address"]['geo']['lat'])
--- Testing.py
+++ Testing.py
@@ -1,3 +1,24 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => console.log(json))
+user = {
+  "id": 1,
+  "name": "Leanne Graham",
+  "username": "Brett",
+  "email": "Sincere@april.biz",
+  "address": {
+    "street": "Kulas Light",
+    "suite": "Apt. 556",
+    "city": "Gwenborough",
+    "zipcode": "92998-3874",
+    "geo": {
+      "lat": "-37.3159",
+      "lng": "81.1496"
+    }
+  },
+  "phone": "1-770-736-8031 x56442",
+  "website": "hildegard.org",
+  "company": {
+    "name": "Romaguera-Crona",
+    "catchPhrase": "Multi-layered client-server neural-net",
+    "bs": "harness real-time e-markets"
+  }
+}
+print(user["address"]["street"])
