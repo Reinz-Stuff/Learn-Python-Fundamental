ae569af644e4e881ac1d4c19c0a401b719c463cb
ACER <aninoem@gmail.com>
2022-10-24 07:50:56 +0700

    revisi data list

--- 6. List Comperhension.py
+++ 6. List Comperhension.py
@@ -1,50 +1,50 @@
-print('\nPerintah del')
+print('\n1.Perintah del')
 daftar_anime = ['Overlord', 'Kingdom', 'Oregairu', 'One Puch Man']
 del daftar_anime[0]
 for i in range(0, len(daftar_anime)):
     print(daftar_anime[i])
 
-print('\nPerintah del dengan list comprehension')
+print('\n2.Perintah del dengan list comprehension')
 daftar_anime = ['Overlord', 'Kingdom', 'Oregairu', 'One Puch Man']
 del daftar_anime[:]
 for i in range(0, len(daftar_anime)):
     print(daftar_anime[i])
 
-print('\nPerintah del dengan list comprehension start')
+print('\n3.Perintah del dengan list comprehension start')
 daftar_anime = ['Overlord', 'Kingdom', 'Oregairu', 'One Puch Man']
 del daftar_anime[0:-2]  # [START:END]
 for i in range(0, len(daftar_anime)):
     print(daftar_anime[i])
 
-print('\nPerintah del dengan list comprehension step')
+print('\n4.Perintah del dengan list comprehension step')
 daftar_anime = ['Overlord', 'Kingdom', 'Oregairu', 'One Puch Man']
 del daftar_anime[0::2]  # [START:END:STEP]
 for i in range(0, len(daftar_anime)):
     print(daftar_anime[i])
 
-print('\nMembuat list baru')
+print('\n5.Membuat list baru')
 daftar_anime = ['Overlord', 'Kingdom', 'Oregairu', 'One Puch Man']
 daftar_anime_baru = daftar_anime[:]  # [START : END]
 for i in range(0, len(daftar_anime)):
     print(daftar_anime[i])
 
-print('\nMembuat list baru')
+print('\n6.Membuat list baru')
 del daftar_anime[:]
 for i in range(0, len(daftar_anime_baru)):
     print(daftar_anime_baru[i])
 
-print('\nMembuat list baru dengan comperhension: Ganjil')
+print('\n7.Membuat list baru dengan comperhension: Ganjil')
 daftar_anime = ['1 Overlord', '2 Kingdom', '3 Oregairu', '4 One Puch Man']
 daftar_anime_baru = daftar_anime[0::2]  # [START : END]
 for i in range(0, len(daftar_anime_baru)):
     print(daftar_anime_baru[i])
 
-print('\nMembuat list baru dengan comperhension: Genap')
+print('\n8.Membuat list baru dengan comperhension: Genap')
 daftar_anime = ['1 Overlord', '2 Kingdom', '3 Oregairu', '4 One Puch Man']
 daftar_anime_baru = daftar_anime[1::2]  # [START : END]
 for i in range(0, len(daftar_anime_baru)):
     print(daftar_anime_baru[i])
 
-print('\nMembuat list baru dengan comperhension: Print')
+print('\n9.Membuat list baru dengan comperhension: Print')
 daftar_anime = ['1 Overlord', '2 Kingdom', '3 Oregairu', '4 One Puch Man']
 print(daftar_anime[0::2])
--- Testing.py
+++ Testing.py
@@ -1,4 +1,15 @@
-daftar_anime = ['Overlord', 'Kingdom', 'Oregairu', 'One Puch Man']
+daftar_list = [1, 2, 3, 4, 5]
 
-for i in range(0, len(daftar_anime)):
-    print(daftar_anime[i])
+daftar_list_baru = daftar_list[2::-2]
+
+print('Daftar list awal')
+for i in range(0, len(daftar_list)):
+    print(daftar_list[i])
+
+del daftar_list[:]
+
+print('Nilai daftar list baru & Hapus daftar list')
+for i in range(0, len(daftar_list_baru)):
+    print(daftar_list_baru[i])
+
+print(daftar_list)
